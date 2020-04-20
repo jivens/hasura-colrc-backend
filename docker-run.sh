7c21538851b4beb9f01a6b7b771fc2b2ce0ddb01
John Ivens <john.wagner.ivens@gmail.com>
2020-04-20 14:02:56 -0700

    Added admin password and paths now work in initialize for all operating systems

--- docker-run.sh
+++ docker-run.sh
@@ -1,4 +1,6 @@
-docker run -d -p 8080:8080 \
-  -e HASURA_GRAPHQL_DATABASE_URL=postgres://username:password@docker.for.win.localhost:port/dbname \
-  -e HASURA_GRAPHQL_ENABLE_CONSOLE=true \
-  hasura/graphql-engine:latest
+docker run -p 8080:8080 \
+    -e HASURA_GRAPHQL_ADMIN_SECRET="myadminsecretkey" \
+    hasura/graphql-engine:latest \
+    graphql-engine \
+    --database-url postgres://postgres:postgrespassword@postgres:5432/postgres \
+    serve
